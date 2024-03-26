e527395f7b982df9ef303bced1f566ee3edf0503
dayeon <dayon0820@naver.com>
2024-03-27 02:50:41 +0900

    세션 과제 제출

--- assignment/assignment/asgi.py
+++ assignment/assignment/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hi project.
+ASGI config for assignment project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment.settings')
 
 application = get_asgi_application()
--- assignment/assignment/urls.py
+++ assignment/assignment/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hi project.
+URL configuration for assignment project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from monster.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello' ,hello),
 ]
